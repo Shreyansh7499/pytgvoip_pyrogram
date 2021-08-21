d8f1d1b6b8306c44e87a7bce02b964afe02e997d
shreyansh7499 <shreyansh17109@iiitd.ac.in>
2021-08-21 16:24:09 +0530

    Update caller.py

--- examples/caller/make_call.py
+++ examples/caller/make_call.py
@@ -1,14 +1,16 @@
 import asyncio
-from .config import *
+from config import *
 from tgvoip import VoIPServerConfig
 from tgvoip_pyrogram import VoIPFileStreamService, VoIPNativeIOService
 import pyrogram
+from pathlib import Path
+import os
 
-
+BASE_DIR = Path(os.path.abspath(os.path.dirname(__file__)))
 VoIPServerConfig.set_bitrate_config(80000, 100000, 60000, 5000, 5000)
 client = pyrogram.Client(session_name = 'session', api_id=API_ID, api_hash=API_HASH, proxy=dict(
         hostname="127.0.0.1",
-        port=9150,
+        port=9050,
         username="",
         password=""))
 
@@ -17,14 +19,14 @@ voip_service = VoIPFileStreamService(client, receive_calls=False)
 
 IN_CALL = True
 
-async def telegram_call():
+async def telegram_call(num_call):
     global IN_CALL
     await client.start()
 
-    call = await voip_service.start_call('@preyansh7499')
-    call.play('audio_files/input.raw')
-    call.play_on_hold(['audio_files/input.raw'])
-    call.set_output_file('audio_files/output.raw')
+    call = await voip_service.start_call('@shreyansh7499')
+    call.play(str(BASE_DIR / 'audio_files/input.raw'))
+    call.play_on_hold([str(BASE_DIR / 'audio_files/input.raw')])
+    call.set_output_file(f'{str(BASE_DIR)}/audio_files/recordings/output{str(num_call)}.raw')
 
     @call.on_call_state_changed
     def state_changed(call, state):
@@ -40,4 +42,4 @@ async def telegram_call():
     while IN_CALL:
         await asyncio.sleep(1)
 
-loop.run_until_complete(telegram_call())
+loop.run_until_complete(telegram_call(1))
